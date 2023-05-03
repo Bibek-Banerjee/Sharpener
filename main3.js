9672fe997ed77c3fa1e8a7948ddf43e29c19ac68
Bibek <vivekbanerjee1996@gmail.com>
2023-05-03 10:25:14 +0530

    del/data/ls

--- main3.js
+++ main3.js
@@ -7,7 +7,9 @@ function local(e){
     var em=document.getElementById("Email").value;
     var nm=document.getElementById("Name").value;
     var pn=document.getElementById("phno").value;
-
+    var del=document.createElement("button");
+    del.id=em;
+    del.innerHTML="delete";
     var myobj={
         nme:nm,
         eml:em,
@@ -17,7 +19,19 @@ function local(e){
     localStorage.setItem(em,newobj);
     var lst=document.getElementById("list");
     var li=document.createElement("li");
+    li.className=em;
     var newItem=em+"-"+nm+'-'+pn;
     li.appendChild(document.createTextNode(newItem));
+    del.addEventListener("click",delt);
+    li.appendChild(del);
+    // console.log(del);
     lst.appendChild(li);
+}
+function delt(e){
+    var iden=this.id;
+    var lis=document.getElementById("list");
+    // var rem=document.getElementsByClassName("iden");
+    localStorage.removeItem(iden);
+    console.log(e.target.parentElement);
+    lis.removeChild(e.target.parentElement);
 }
